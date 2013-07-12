c57c7d237ebe550e7e410ed25109efe8d77eb8e9
tailang <caopingcpu@163.com>
2013-07-12 14:20:33 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tblog::Application.config.secret_token = '467898305dfba2ef7f0372d7c7f9625126e7f2c1b659fb491a0f7e7c77ae898a4a16d009a144a83e5c071c1f0e6a002b021b64d31b46c38447d911e6a030e36c'
