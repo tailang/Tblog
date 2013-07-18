10a04d345583a62286d7c057d241e910ea3d644a
tailang <caopingcpu@163.com>
2013-07-18 21:44:22 +0800

    fix bug

--- Gemfile
+++ Gemfile
@@ -16,9 +16,7 @@ end
 
 gem 'jquery-rails'
 
-group :production do
-	gem 'mysql2'
-end
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130715064831) do
+ActiveRecord::Schema.define(:version => 20130718133059) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""
